c2df80a4b311711a9b5660f55d1bcbe43d325950
Coral Fussman <coralfussman@gmail.com>
2020-03-21 11:36:56 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -14,4 +14,15 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
+
+  row_index = 0
+  while row_index < print_first_directors_movie_titles.count do
+    element_index = 0
+    while element_index < print_first_directors_movie_titles[row_index].count do
+      puts print_first_directors_movie_titles[row_index][element_index]
+      element_index +=1
+
+    end
+    row_index +=1
+  end
 end
